b04399b6ef4323c207c850deae3695e9879eeaa6
csm <csm009177@gmail.com>
2023-11-09 19:12:46 +0900

    success link server with csv file

--- app.js
+++ app.js
@@ -3,39 +3,33 @@ const http = require('http');
 const fs = require('fs');
 const parse = require('csv-parse');
 
+// port
+const port = 3000;
+
+// server
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/') {
-    // index.html 파일 읽어서 응답
+    // read index.html file and response
     fs.readFile('index.html', 'utf8', (err, data) => {
       if (err) {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Internal Server Error');
         return;
       }
-
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(data);
     });
-  } else if (req.method === 'GET' && req.url === '/data') {
-    // CSV 파일 읽어서 JSON 형태로 클라이언트에 전송
+  } else if (req.method === 'GET' && req.url === '/data.csv') {
+    // read CSV file and send to client as text
     fs.readFile('data.csv', 'utf8', (err, csvData) => {
       if (err) {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Internal Server Error');
         return;
       }
-      // CSV 파싱
-      parse(csvData, { columns: true }, (err, records) => {
-        if (err) {
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('Internal Server Error');
-          return;
-        }
-
-        // JSON 형태로 응답
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(records));
-      });
+      // send CSV data as text
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(csvData);
     });
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
@@ -43,8 +37,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const port = 3000;
-
+// listen
 server.listen(port, () => {
-  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
+  console.log(`http://localhost:${port}`);
 });
